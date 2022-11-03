e5542bd7cdd5f9da4ab275fb4084781c5c063e16
bigvvill <wcurtisusa@gmail.com>
2022-11-03 07:59:10 -0400

    Add project files.

---
+++ DrinksInfoApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+
+using HttpClient client = new();
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json"));
+//client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+await ProcessRepositoriesAsync(client);
+
+static async Task ProcessRepositoriesAsync(HttpClient client)
+{
+    var json = await client.GetStringAsync(
+         "https://www.thecocktaildb.com/api/json/v1/1/random.php");
+
+    Console.Write(json);
+}
